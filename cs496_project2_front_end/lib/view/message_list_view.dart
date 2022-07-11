b848803dd0ead54b2b9c25aea989cd2e10298a29
jaduksuh <jaduksuh>
2022-07-11 20:06:46 +0900

    Started adding message lists

--- cs496_project2_front_end/lib/view/message_list_view.dart
+++ cs496_project2_front_end/lib/view/message_list_view.dart
@@ -1,3 +1,5 @@
+import 'package:cs496_project2_front_end/model/message_model.dart';
+import 'package:cs496_project2_front_end/viewmodel/message_viewmodel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,8 +15,49 @@ class MessageListView extends StatelessWidget {
             title: const Text('메세지 목록'),
             elevation: 0.0,
           ),
+          body: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              height: 120,
+              child: FutureBuilder<List<MessageModel>>(
+                future: fetchMessages(),
+                builder: ((context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData &&
+                      snapshot.data!.isNotEmpty) {
+                    return ListView.separated(
+                        scrollDirection: Axis.vertical,
+                        itemBuilder: ((context, index) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(3.0)),
+                              child: Row(
+                                children: [
+                                  // ClipRRect(
+                                  //   borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                  //   child: Image.network(src)
+                                  // )
+                                ],
+                              ));
+                        }),
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(height: 10),
+                        itemCount: snapshot.data!.length);
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                }),
+              )),
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => {},
+          ),
         );
       },
     );
   }
 }
+
+// class ProfileImage extends 
